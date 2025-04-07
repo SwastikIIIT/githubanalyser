31ef5dfa6cff0beb24cd79aceee7e5caec7a99a6
Swastik Sharma <153355864+SwastikIIIT@users.noreply.github.com>
2025-04-08 01:31:55 +0530

    Update services.ts

--- src/services.ts
+++ src/services.ts
@@ -87,7 +87,7 @@ const headers = {
 
 export const fetchUserData=async(username:string)=>{
   try {
-    const response=await fetch(`https://api.github.com/users/${username}`);
+    const response=await fetch(`https://api.github.com/users/${username}`,{headers});
     if(!response.ok) {
       return {success:false,message:'User not found'};
     }
@@ -102,7 +102,7 @@ export const fetchUserData=async(username:string)=>{
 
 export const fetchUserRepos=async(username:string)=>{
   try {
-    const response = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const response = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`,{headers});
     if(!response.ok)
     return {success:false,message:'No repo found'};
     const result=await response.json();
@@ -163,7 +163,7 @@ export const processCommits=(pushEvents:GitHubEvent[],createEvents:GitHubEvent[]
 
  export const fetchEvents=async(username:string)=>{
       try{
-          const response=await fetch(`https://api.github.com/users/${username}/events?per_page=100`);
+          const response=await fetch(`https://api.github.com/users/${username}/events?per_page=100`,{headers});
           if(!response.ok) {
             return {success:false,message:'Events not found'};
           }
